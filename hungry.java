271ae6cd3a62351db57cde4e5693b4b654c273c9
Shubham Shejwal <shubhamshejwal932002@gmail.com>
2024-01-31 20:06:51 +0530

    program now takes input from user to show message

--- hungry.java
+++ hungry.java
@@ -1,6 +1,13 @@
+import java.util.*;
 public class hungry {
     public static void main(String []args)
     {
-        System.out.println("i am hungry");
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Are u hungry?");
+        String s= sc.nextLine();
+        if(s.equals("yes"))
+            System.out.println("eat pizza");
+        else
+            System.out.println("do your homework");
     }
 }
